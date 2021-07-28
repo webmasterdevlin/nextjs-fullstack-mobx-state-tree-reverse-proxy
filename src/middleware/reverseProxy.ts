f1d47af77fa402214e8a8d78cfb83bdcc678b82d
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-07-28 08:49:39 +0200

    updated

--- src/middleware/reverseProxy.ts
+++ src/middleware/reverseProxy.ts
@@ -1,18 +1,18 @@
 import httpProxyMiddleware from "next-http-proxy-middleware";
 
-const reverseProxy = (handler) => async (req, res) => {
+const reverseProxy = async (req, res, next) => {
   process && process.env.NODE_ENV === "development"
     ? await httpProxyMiddleware(req, res, {
         // You can use the `http-proxy` option
         target: "https://jsonplaceholder.typicode.com",
         // In addition, you can use the `pathRewrite` option provided by `next-http-proxy`
         pathRewrite: {
-          "^/api": "",
+          "^/api": "/",
         },
       })
     : res.status(404).send(null);
 
-  return handler(req, res);
+  return next();
 };
 
 export default reverseProxy;
--- src/pages/api/posts/[id].ts
+++ src/pages/api/posts/[id].ts
@@ -2,6 +2,6 @@ import nc from "next-connect";
 
 import reverseProxy from "src/middleware/reverseProxy";
 
-const handler = nc();
+const handler = nc().use(reverseProxy);
 
-export default reverseProxy(handler);
+export default handler;
